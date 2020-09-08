1d67a7aad0a4164181748c9729b3def96ca14e04
Joykumar1160 <70939292+Joykumar1160@users.noreply.github.com>
2020-09-08 04:50:27 -0700

    my project
    This is full project

---
+++ js/custombarfiller.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('#bar1').barfiller({barColor:'#1AB394', duration:'4000'});
+});
+$(document).ready(function () {
+    $('#bar2').barfiller({barColor:'#1AB394', duration:'4000'});
+});
+$(document).ready(function () {
+    $('#bar3').barfiller({barColor:'#1AB394', duration:'4000'});
+});
+$(document).ready(function () {
+    $('#bar4').barfiller({barColor:'#1AB394', duration:'4000'});
+});
+$(document).ready(function () {
+    $('#bar5').barfiller({barColor:'#1AB394', duration:'4000'});
+});
+$(document).ready(function () {
+    $('#bar6').barfiller({barColor:'#1AB394', duration:'4000'});
+});
